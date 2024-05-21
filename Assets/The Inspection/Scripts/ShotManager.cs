b799d999b2be83ec30384c6766f3a78c0c32c28e
Nicholas Lever <nik@catalystpics.co.uk>
2024-05-21 10:28:14 +0100

    first commit

---
+++ Assets/The Inspection/Scripts/ShotManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class ShotManager : MonoBehaviour
+{
+	public CinemachineCamera activeVCam;
+
+    public void SetShot(CinemachineCamera newVCam)
+	{
+		activeVCam.Priority = 0;
+		newVCam.Priority = 100;
+
+		activeVCam = newVCam;
+	}
+}
---
+++ Assets/The Inspection/Scripts/VCamArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class VCamArea : MonoBehaviour
+{
+    public CinemachineCamera virtualCamera;
+	private ShotManager shotManager;
+
+	private void Start()
+	{
+		shotManager = GameObject.FindFirstObjectByType<ShotManager>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		shotManager.SetShot(virtualCamera);
+	}
+}
